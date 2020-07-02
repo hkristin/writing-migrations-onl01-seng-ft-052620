78c9982fea189b0b3f49deb27563fa4289afc39c
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-02 17:20:56 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,8 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
   def change
-    change_column :students, :table_name, :string
-    change_column :students, :column_name, :string
-    change_column :students, :type, :string
+    change_column :students, :birthdate, :string
   end
 end
